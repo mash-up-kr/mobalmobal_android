394c6c315d2dba5e5918121a9e8554b7af7605df
Jaemin.Park <once29@naver.com>
2021-02-14 00:57:26 +0900

    Implement Hilt

--- app/src/main/java/com/mashup/mobalmobal/ui/MobalApplication.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/MobalApplication.kt
@@ -1,7 +1,8 @@
-package com.mashup.mobalmobal
+package com.mashup.mobalmobal.ui
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.mashup.mobalmobal.data.sharedpreferences.MobalSharedPreferencesImpl
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,9 +11,14 @@ class MobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initializeStetho()
+        initializeSharedPreferences()
     }
 
     private fun initializeStetho() {
         Stetho.initializeWithDefaults(this)
+    }
+
+    private fun initializeSharedPreferences() {
+        MobalSharedPreferencesImpl.init(this)
     }
 }

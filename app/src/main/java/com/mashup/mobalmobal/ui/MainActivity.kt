f695ea3fe888c811b25966b280a78cba76e39188
hugh <btyisu@afreecatv.com>
2021-02-14 00:15:10 +0900

    add firebase message dependency

--- app/src/main/java/com/mashup/mobalmobal/ui/MainActivity.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.mashup.mobalmobal.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.mashup.mobalmobal.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, "token : $token")
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
     }
 }

174466d4a862f06740d476091ff6951990d8e457
Jaemin.Park <once29@naver.com>
2021-02-14 00:59:09 +0900

    Implement MobalDatabase

---
+++ app/src/main/java/com/mashup/mobalmobal/data/MobalDatabase.kt
@@ -0,0 +1,55 @@
+package com.mashup.mobalmobal.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.funin.base.funinbase.BuildConfig
+import com.mashup.mobalmobal.data.dao.DonationDao
+import com.mashup.mobalmobal.data.dao.UserDao
+import com.mashup.mobalmobal.data.vo.Donation
+import com.mashup.mobalmobal.data.vo.User
+
+@Database(
+    entities = [
+        User::class,
+        Donation::class
+    ],
+    version = 1
+)
+abstract class MobalDatabase : RoomDatabase() {
+
+    companion object {
+        private const val IS_CLEAR_ALL = false
+
+        private const val DM_NAME = "mobal.db"
+
+        @Volatile
+        private var INSTANCE: MobalDatabase? = null
+
+        fun getInstance(context: Context): MobalDatabase = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+        }
+
+        private fun buildDatabase(context: Context): MobalDatabase =
+            Room.databaseBuilder(context, MobalDatabase::class.java, DM_NAME)
+                .fallbackToDestructiveMigration()
+                .apply {
+                    if (BuildConfig.DEBUG && IS_CLEAR_ALL) {
+                        addCallback(CALLBACK_CLEAR_ALL)
+                    }
+                }
+                .build()
+
+        private val CALLBACK_CLEAR_ALL = object : Callback() {
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                // TODO
+            }
+        }
+    }
+
+    abstract fun donationDao(): DonationDao
+    abstract fun userDao(): UserDao
+}

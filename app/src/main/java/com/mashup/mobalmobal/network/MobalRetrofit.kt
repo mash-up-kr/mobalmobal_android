06050a9ba3d834b64693bc6b6aa85f646a5ee6ea
Jaemin.Park <once29@naver.com>
2021-02-14 00:58:25 +0900

    Add base network setting

---
+++ app/src/main/java/com/mashup/mobalmobal/network/MobalRetrofit.kt
@@ -0,0 +1,33 @@
+package com.mashup.mobalmobal.network
+
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object MobalRetrofit {
+
+    // TODO Replace end-point
+    const val API_END_POINT = "https://example.com/api"
+
+    fun <T> create(
+        service: Class<T>,
+        client: OkHttpClient,
+        httpUrl: String = API_END_POINT
+    ): T = Retrofit.Builder()
+        .baseUrl(httpUrl)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+        .build()
+        .create(service)
+
+    inline fun <reified T : Any> create(
+        client: OkHttpClient,
+        httpUrl: String = API_END_POINT
+    ): T {
+        require(httpUrl.isNotBlank()) { "Parameter httpUrl cannot be blank." }
+        return create(service = T::class.java, httpUrl = httpUrl, client = client)
+    }
+}

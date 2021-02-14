44a4617f957dcf6ea5cd967733b50b3cdf0f3afd
koba <koba1mobile@gmail.com>
2021-02-15 00:19:29 +0900

    Add function to move fragment to fragment

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/ProfileFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.funin.base.funinbase.base.BaseFragment
 import com.mashup.mobalmobal.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,4 +21,10 @@ class ProfileFragment : BaseFragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
+    private fun goProfileToMyDonations() =
+        findNavController().navigate(R.id.action_profileFragment_to_myDonationsFragment)
+
+    private fun goProfileToSettings() =
+        findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
 }

95cbb1ea72149fae32a071db136f40e2643c99dd
jin5578 <jin55789@naver.com>
2021-02-21 14:34:27 +0900

    🚀 Add viewbinding and temp design for SignInFragment

--- app/src/main/java/com/mashup/mobalmobal/ui/sign/signin/SignInFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/sign/signin/SignInFragment.kt
@@ -2,7 +2,6 @@ package com.mashup.mobalmobal.ui.sign.signin
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.funin.base.funinbase.base.BaseViewBindingFragment
 import com.mashup.mobalmobal.R
 import com.mashup.mobalmobal.databinding.FragmentSignInBinding
@@ -11,6 +10,10 @@ import com.mashup.mobalmobal.databinding.FragmentSignInBinding
  * 로그인
  */
 class SignInFragment : BaseViewBindingFragment<FragmentSignInBinding>() {
+
+    companion object {
+        private const val TAG = "SignInFragment"
+    }
 
     override fun setBinding(
         inflater: LayoutInflater,

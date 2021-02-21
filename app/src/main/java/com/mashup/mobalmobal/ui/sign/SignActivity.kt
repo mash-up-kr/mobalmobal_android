9e9e5fe6611b1e7e8fcde6cf489dc0cde9047f67
jin5578 <jin55789@naver.com>
2021-02-21 14:29:49 +0900

    🚀 Add viewbinding for SignActivity

--- app/src/main/java/com/mashup/mobalmobal/ui/sign/SignActivity.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/sign/SignActivity.kt
@@ -1,5 +1,20 @@
 package com.mashup.mobalmobal.ui.sign
 
-import com.funin.base.funinbase.base.BaseActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mashup.mobalmobal.databinding.ActivitySignBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class SignActivity : BaseActivity()
+
+@AndroidEntryPoint
+class SignActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}

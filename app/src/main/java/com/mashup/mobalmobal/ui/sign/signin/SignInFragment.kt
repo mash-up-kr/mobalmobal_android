f253610c5024e774c2aa54c01e620b2bfe2e74d0
jin5578 <jin55789@naver.com>
2021-02-21 18:00:13 +0900

    🚀 Add function Google login

--- app/src/main/java/com/mashup/mobalmobal/ui/sign/signin/SignInFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/sign/signin/SignInFragment.kt
@@ -1,8 +1,17 @@
 package com.mashup.mobalmobal.ui.sign.signin
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
 import com.funin.base.funinbase.base.BaseViewBindingFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.mashup.mobalmobal.R
 import com.mashup.mobalmobal.databinding.FragmentSignInBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,11 +26,69 @@ class SignInFragment : BaseViewBindingFragment<FragmentSignInBinding>() {
         private const val TAG = "SignInFragment"
     }
 
+    private val viewModel by viewModels<SignInViewModel>()
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var googleLoginLauncher: ActivityResultLauncher<Intent>
+
     override fun setBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentSignInBinding = FragmentSignInBinding.inflate(inflater, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setGoogleLogin()
+
+        binding.signInGoogle.setOnClickListener {
+            onGoogleClicked()
+        }
+
+        viewModel.showToast.observe(viewLifecycleOwner, { message ->
+            showToastMessage(message)
+        })
+    }
+
+    private fun setGoogleLogin() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        googleLoginLauncher = registerForActivityResult(GoogleLoginResultContract()) { idToken ->
+            idToken?.let {
+                viewModel.signInFirebase(it)
+            }
+        }
+    }
+
+    private fun onGoogleClicked() {
+        googleLoginLauncher.launch(googleSignInClient.signInIntent)
+    }
+
     fun navigateSignInToSignUp() =
         findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+
+    inner class GoogleLoginResultContract : ActivityResultContract<Intent, String>() {
+        override fun createIntent(context: Context, input: Intent): Intent = input
+
+        override fun parseResult(resultCode: Int, intent: Intent?): String {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                account?.let {
+                    return account.idToken!!
+                }
+            } catch (e: ApiException) {
+                e.printStackTrace()
+            }
+
+            return ""
+        }
+    }
 }
--- app/src/main/java/com/mashup/mobalmobal/ui/sign/signin/SignInViewModel.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/sign/signin/SignInViewModel.kt
@@ -1,7 +1,13 @@
 package com.mashup.mobalmobal.ui.sign.signin
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.funin.base.funinbase.base.BaseViewModel
 import com.funin.base.funinbase.rx.schedulers.BaseSchedulerProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -13,6 +19,27 @@ class SignInViewModel @Inject constructor(
 
     companion object {
         private const val TAG = "SignInViewModel"
+    }
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val _showToast = MutableLiveData<String>()
+    val showToast: LiveData<String>
+        get() = _showToast
+
+
+    fun signInFirebase(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        auth.signInWithCredential(credential).addOnCompleteListener() { task ->
+            if (task.isSuccessful) {
+                auth.currentUser?.let {
+                    //서버 키 체크 Logic (우리 서비스 가입 여부 확인)
+                }
+            } else {
+                _showToast.postValue("구글 로그인에 실패하였습니다.")
+            }
+        }
     }
 
 }

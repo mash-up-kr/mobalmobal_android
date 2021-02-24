a1d8242d4e8875eb6a11eb6be5494f9eb5db7adc
Jaemin.Park <once29@naver.com>
2021-02-25 00:29:02 +0900

    🚀 Fix fragments to extend BaseViewBindingFragment

--- app/src/main/java/com/mashup/mobalmobal/ui/mydonations/MyDonationsFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/mydonations/MyDonationsFragment.kt
@@ -1,23 +1,18 @@
 package com.mashup.mobalmobal.ui.mydonations
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.funin.base.funinbase.base.BaseFragment
-import com.mashup.mobalmobal.R
+import com.funin.base.funinbase.base.BaseViewBindingFragment
+import com.mashup.mobalmobal.databinding.FragmentMyDonationsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * 나의 도네이션 페이지
  */
 @AndroidEntryPoint
-class MyDonationsFragment : BaseFragment() {
-    override fun onCreateView(
+class MyDonationsFragment : BaseViewBindingFragment<FragmentMyDonationsBinding>() {
+    override fun setBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_my_donations, container, false)
-    }
+        container: ViewGroup?
+    ): FragmentMyDonationsBinding = FragmentMyDonationsBinding.inflate(inflater, container, false)
 }

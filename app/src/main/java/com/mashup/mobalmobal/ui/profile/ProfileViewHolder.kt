b365744676073fec23bac77991ab52ff5ab63ace
koba <koba1mobile@gmail.com>
2021-02-25 03:27:35 +0900

    🚀 Add 'HiltViewModel' annotation and 'getProfile' function

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/ProfileViewHolder.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/ProfileViewHolder.kt
@@ -3,14 +3,16 @@ package com.mashup.mobalmobal.ui.profile
 import com.funin.base.funinbase.base.BaseViewModel
 import com.funin.base.funinbase.rx.schedulers.BaseSchedulerProvider
 import com.mashup.mobalmobal.ui.profile.data.repository.ProfileRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-class ProfileViewHolder(
-    @Inject
+@HiltViewModel
+class ProfileViewHolder @Inject constructor(
     var schedulerProvider: BaseSchedulerProvider,
-    @Inject
     var profileRepository: ProfileRepository
 ) : BaseViewModel(schedulerProvider){
 
-
+    fun getProfile(userId: String) =
+        profileRepository.getProfile(userId)
+            .subscribeOnIO()
 }

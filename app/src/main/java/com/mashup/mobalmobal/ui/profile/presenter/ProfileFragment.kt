d62b9855a6db950e7926ae91902c3a6013b48b0b
koba <koba1mobile@gmail.com>
2021-02-26 04:57:34 +0900

    🚀 Add ProfileRecyclerView and Observe profile subeject for submit list

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/ProfileFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.mashup.mobalmobal.ui.profile.presenter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.funin.base.funinbase.base.BaseViewBindingFragment
 import com.funin.base.funinbase.extension.rx.observeOnMain
 import com.funin.base.funinbase.extension.rx.subscribeWithErrorLogger
@@ -13,9 +16,6 @@ import com.mashup.mobalmobal.R
 import com.mashup.mobalmobal.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * 프로필 변경
- */
 @AndroidEntryPoint
 class ProfileFragment : BaseViewBindingFragment<FragmentProfileBinding>() {
     companion object {
@@ -23,8 +23,8 @@ class ProfileFragment : BaseViewBindingFragment<FragmentProfileBinding>() {
     }
 
     private val profileViewModel: ProfileViewModel by viewModels()
-
-    val userId: String by lazy {
+    private val profileAdapter by lazy { ProfileAdapter() }
+    private val userId: String by lazy {
         arguments?.getString(KEY_USER_ID)
             ?: throw IllegalArgumentException("ProfileFragment must have user's id")
     }
@@ -34,17 +34,30 @@ class ProfileFragment : BaseViewBindingFragment<FragmentProfileBinding>() {
         container: ViewGroup?
     ): FragmentProfileBinding = FragmentProfileBinding.inflate(inflater, container, false)
 
+    override fun onSetupViews(view: View) {
+        super.onSetupViews(view)
+        binding.recyclerViewProfile.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = profileAdapter
+        }
+    }
+
     override fun onBindViewModels() {
         super.onBindViewModels()
         observeViewModel()
+        requestUserProfile()
     }
+
+    private fun requestUserProfile() = profileViewModel.getProfile(userId)
 
     private fun observeViewModel() = with(profileViewModel) {
         toastSubject.observeOnMain()
             .subscribeWithErrorLogger { context?.showToast(it, Toast.LENGTH_SHORT) }
+            .addToDisposables()
 
         profileSubject.observeOnMain()
-            .subscribeWithErrorLogger {}
+            .subscribeWithErrorLogger { profileAdapter.submitList(it) }
+            .addToDisposables()
     }
 
     private fun navigateProfileToMyDonations() =

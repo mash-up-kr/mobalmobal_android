56f32e2823ccf184198186bedc284f453d45d0b9
koba <koba1mobile@gmail.com>
2021-02-26 03:20:09 +0900

    🚀 Add Repository, Scehdulers Module

---
+++ app/src/main/java/com/mashup/mobalmobal/di/SchedulersModule.kt
@@ -0,0 +1,16 @@
+package com.mashup.mobalmobal.di
+
+import com.funin.base.funinbase.rx.schedulers.BaseSchedulerProvider
+import com.funin.base.funinbase.rx.schedulers.SchedulerProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SchedulersModule {
+
+    @Provides
+    fun provideSchedulerProvider(): BaseSchedulerProvider = SchedulerProvider
+}

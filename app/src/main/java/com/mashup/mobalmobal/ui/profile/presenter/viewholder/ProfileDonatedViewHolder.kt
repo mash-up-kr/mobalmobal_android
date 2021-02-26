0f66a865d75a0f265cecf77534de707cc91c0bc1
koba <koba1mobile@gmail.com>
2021-02-27 00:08:01 +0900

    🎨 Update profile donated, request donation view

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/viewholder/ProfileDonatedViewHolder.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/viewholder/ProfileDonatedViewHolder.kt
@@ -2,13 +2,16 @@ package com.mashup.mobalmobal.ui.profile.presenter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.mashup.mobalmobal.databinding.HolderProfileDonatedBinding
 import com.mashup.mobalmobal.ui.profile.domain.model.ProfileItem
 
 class ProfileDonatedViewHolder(
-    private val binding: ViewBinding
+    private val binding: HolderProfileDonatedBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+
     fun bind(item: ProfileItem.Donated) {
         with(binding) {
+            tvTitle.text = item.title
         }
     }
 }

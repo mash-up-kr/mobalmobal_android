e1f03c812335ae27366f0272d73766806aa64f17
koba <koba1mobile@gmail.com>
2021-02-26 23:30:02 +0900

    🎨 Update profile point view

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/viewholder/ProfileUserViewHolder.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/viewholder/ProfileUserViewHolder.kt
@@ -1,8 +1,6 @@
 package com.mashup.mobalmobal.ui.profile.presenter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.mashup.base.image.GlideRequest
 import com.mashup.base.image.GlideRequests
 import com.mashup.mobalmobal.databinding.HolderProfileUserBinding
 import com.mashup.mobalmobal.ui.profile.domain.model.ProfileItem
@@ -13,14 +11,14 @@ class ProfileUserViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     @Inject
-    lateinit var requests: GlideRequests
+    lateinit var glideRequest: GlideRequests
 
     fun bind(item: ProfileItem.User) {
         with(binding) {
             tvProfileName.text = item.name
             tvProfileNickName.text = item.nickName
 
-            requests.load(item.profileUrl)
+            glideRequest.load(item.profileUrl)
                 .centerCrop()
                 .into(ivProfile)
         }

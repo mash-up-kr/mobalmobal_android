a2d866fae8b4b7a5d0baf5445ad03ae2f9f55b89
koba <koba1mobile@gmail.com>
2021-02-28 00:36:49 +0900

    🎨 Update profile user and link binding

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/viewholder/ProfileUserViewHolder.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/presenter/viewholder/ProfileUserViewHolder.kt
@@ -1,15 +1,28 @@
 package com.mashup.mobalmobal.ui.profile.presenter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mashup.base.image.GlideRequest
+import com.mashup.base.image.GlideRequests
 import com.mashup.mobalmobal.databinding.HolderProfileUserBinding
 import com.mashup.mobalmobal.ui.profile.domain.model.ProfileItem
+import javax.inject.Inject
 
 class ProfileUserViewHolder(
     private val binding: HolderProfileUserBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+
+    @Inject
+    lateinit var requests: GlideRequests
+
     fun bind(item: ProfileItem.User) {
         with(binding) {
-            profileName.text = item.name
+            tvProfileName.text = item.name
+            tvProfileNickName.text = item.nickName
+
+            requests.load(item.profileUrl)
+                .centerCrop()
+                .into(ivProfile)
         }
     }
 }

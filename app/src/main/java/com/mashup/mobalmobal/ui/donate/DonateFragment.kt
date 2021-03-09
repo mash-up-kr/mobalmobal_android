2fe3cbe83b1194a936fd7deb49044c485c23d8e1
현성 <jin55789@naver.com>
2021-03-09 18:14:35 +0900

    🚀 Add donation logic

--- app/src/main/java/com/mashup/mobalmobal/ui/donate/DonateFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/donate/DonateFragment.kt
@@ -1,21 +1,37 @@
 package com.mashup.mobalmobal.ui.donate
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.funin.base.funinbase.base.BaseViewBindingFragment
 import com.mashup.mobalmobal.R
 import com.mashup.mobalmobal.databinding.FragmentDonateBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DonateFragment : BaseViewBindingFragment<FragmentDonateBinding>() {
-    private val donateViewModel: DonateViewModel by viewModels()
+    private val donateViewModel by viewModels<DonateViewModel>()
 
     override fun setBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentDonateBinding = FragmentDonateBinding.inflate(inflater, container, false)
-    
+
+    override fun onSetupViews(view: View) {
+        super.onSetupViews(view)
+
+        binding.tvDonate.setOnClickListener {
+            val price = binding.etDonatePrice.text.toString()
+            donateViewModel.requestDonation(price)
+        }
+    }
+
+    override fun onBindViewModels() {
+        super.onBindViewModels()
+    }
+
     private fun navigateDonateToCharge() =
         findNavController().navigate(R.id.action_donateFragment_to_chargeFragment)
 }
--- app/src/main/java/com/mashup/mobalmobal/ui/donate/DonateViewModel.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/donate/DonateViewModel.kt
@@ -1,5 +1,6 @@
 package com.mashup.mobalmobal.ui.donate
 
+import android.util.Log
 import com.funin.base.funinbase.base.BaseViewModel
 import com.funin.base.funinbase.rx.schedulers.BaseSchedulerProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,5 +10,9 @@ import javax.inject.Inject
 class DonateViewModel @Inject constructor(
     schedulerProvider: BaseSchedulerProvider
 ) : BaseViewModel(schedulerProvider) {
+
+    fun requestDonation(price: String) {
+        //Api 로직 처리 예정
+    }
 
 }

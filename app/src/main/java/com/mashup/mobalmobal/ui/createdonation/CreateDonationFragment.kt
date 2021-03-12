705108bd77554c05290f7232fbaff44bcba52717
Huijiny <jiin20803@gmail.com>
2021-03-13 02:25:36 +0900

    🚀Add TextWatcher for thousand comma and Add TedImagePicker Library & Glide for ImageView

--- app/src/main/java/com/mashup/mobalmobal/ui/createdonation/CreateDonationFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/createdonation/CreateDonationFragment.kt
@@ -1,21 +1,25 @@
 package com.mashup.mobalmobal.ui.createdonation
 
 import android.animation.ObjectAnimator
-import android.util.Log
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.core.animation.doOnEnd
-import androidx.core.view.isVisible
 import com.funin.base.funinbase.base.BaseViewBindingFragment
-import com.funin.base.funinbase.extension.dpToPixelsAsFloat
+import com.mashup.base.image.GlideRequests
 import com.mashup.mobalmobal.databinding.FragmentCreateDonationBinding
 import dagger.hilt.android.AndroidEntryPoint
-import com.funin.base.funinbase.extension.showToast
+import gun0912.tedimagepicker.builder.TedRxImagePicker
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CreateDonationFragment : BaseViewBindingFragment<FragmentCreateDonationBinding>() {
+    @Inject
+    lateinit var glideRequest: GlideRequests
 
     override fun setBinding(
         inflater: LayoutInflater,
@@ -25,6 +29,40 @@ class CreateDonationFragment : BaseViewBindingFragment<FragmentCreateDonationBin
 
     override fun onSetupViews(view: View) {
         setActionListenerAtCreateDonationInputs()
+        setImageFromGallery()
+        setEditTextCommaSeparateListener()
+    }
+
+    private fun setImageFromGallery() {
+        binding.createDonationProductImageView.setOnClickListener {
+            TedRxImagePicker.with(requireActivity())
+                .start()
+                .subscribe({ uri ->
+                    glideRequest.load(uri)
+                        .centerCrop()
+                        .into(binding.createDonationProductImageView)
+                }, Throwable::printStackTrace)
+        }
+    }
+
+    private fun setEditTextCommaSeparateListener() {
+        binding.createDonationPriceInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (!TextUtils.isEmpty(s.toString()) && s.toString() != "") {
+                    binding.createDonationPriceInput.also {
+                        val number = String.format("%,d", s.toString().replace(",", "").toLong())
+                        it.removeTextChangedListener(this)
+                        it.setText(number)
+                        it.addTextChangedListener(this)
+                        it.setSelection(number.length)
+                    }
+                }
+            }
+        })
     }
 
     private fun setActionListenerAtCreateDonationInputs() {
@@ -46,6 +84,7 @@ class CreateDonationFragment : BaseViewBindingFragment<FragmentCreateDonationBin
 
             false
         }
+
         binding.createDonationDescriptionInput.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_GO && binding.createDonationPriceTextInputLayout.visibility != View.VISIBLE) {
                 binding.createDonationPriceInput.requestFocus()

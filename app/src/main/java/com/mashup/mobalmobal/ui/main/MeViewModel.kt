42024b9362779aab1dceef7821cc485a3a401cec
Jaemin.Park <once29@naver.com>
2021-04-02 00:32:50 +0900

    🏗️ Fix MainActivity to check signed in

--- app/src/main/java/com/mashup/mobalmobal/ui/main/MeViewModel.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/main/MeViewModel.kt
@@ -1,6 +1,5 @@
 package com.mashup.mobalmobal.ui.main
 
-import android.util.Log
 import com.funin.base.funinbase.base.BaseViewModel
 import com.funin.base.funinbase.rx.schedulers.BaseSchedulerProvider
 import com.mashup.mobalmobal.data.sharedpreferences.MobalSharedPreferences
@@ -14,7 +13,7 @@ class MeViewModel @Inject constructor(
     private val sharedPreferences: MobalSharedPreferences
 ) : BaseViewModel(schedulerProvider) {
 
-    fun checkSignedIn(): Single<Boolean> = sharedPreferences.getAccessTokenFlowable().firstOrError()
+    fun isSignedIn(): Single<Boolean> = sharedPreferences.getAccessTokenFlowable().firstOrError()
         .map { it.isNotBlank() }
 
 }

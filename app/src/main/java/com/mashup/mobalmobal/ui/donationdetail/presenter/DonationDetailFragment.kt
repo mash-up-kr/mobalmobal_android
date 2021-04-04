0f70c963f105b2cd6db87da6d4bcdd66268b0926
koba <koba1mobile@gmail.com>
2021-04-05 01:28:43 +0900

    💄 Update donation detail view

--- app/src/main/java/com/mashup/mobalmobal/ui/donationdetail/presenter/DonationDetailFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/donationdetail/presenter/DonationDetailFragment.kt
@@ -1,8 +1,17 @@
 package com.mashup.mobalmobal.ui.donationdetail.presenter
 
 import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.funin.base.funinbase.base.BaseViewBindingFragment
@@ -54,6 +63,28 @@ class DonationDetailFragment : BaseViewBindingFragment<FragmentDetailBinding>() 
     }
 
     private fun bindDonation(donation: DonationItem) = with(binding) {
+        val donationTitle = getString(
+            R.string.donation_detail_title,
+            donation.author.nickName,
+            donation.productName
+        )
+
+        tvDonationTitle.text = SpannableStringBuilder(donationTitle).apply {
+            setSpan(
+                RelativeSizeSpan(1.15f),
+                donationTitle.indexOf(donation.author.nickName),
+                donationTitle.indexOf(donation.author.nickName) + donation.author.nickName.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            setSpan(
+                ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.light_yellow)),
+                donationTitle.indexOf(donation.productName),
+                donationTitle.indexOf(donation.productName) + donation.productName.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
         glideRequests.load(donation.imageUrl)
             .into(ivProduct)
 
@@ -61,31 +92,24 @@ class DonationDetailFragment : BaseViewBindingFragment<FragmentDetailBinding>() 
             .circleCrop()
             .into(ivProfile)
 
-        tvDonationTitle.text = getString(
-            R.string.donation_detail_title,
-            donation.author.nickName,
-            donation.productName
-        )
         tvDonationDescription.text = donation.description
         tvGoalPrice.text = getString(R.string.donation_price, donation.goalPrice)
         tvDonationCurrnetPrice.text = getString(R.string.donation_price, donation.donatedPrice)
         tvDonationEndDate.text = donation.dueDate.toString()
         tvDonationPercent.text = getString(R.string.donation_percent, donation.donatePercent)
-
+        progressDonating.progress = donation.donatePercent.toInt()
         tvDonator.text = if(donation.donators.isNotEmpty()) {
             getString(R.string.donation_detail_donator, donation.donators.size)
         }else {
             getString(R.string.donation_detail_donator_empty)
         }
         tvFunding.text = getString(R.string.donation_detail_funding, donation.author.nickName)
-
         vDonator.apply {
             setDonatorProfiles(
                 glideRequests,
                 donation.donators.map { it.profileUrl }
             )
         }
-
     }
 
     private fun requestDonationDetail(donationId: Int) =

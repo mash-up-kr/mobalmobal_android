9fb06bd62d22e56000c89701af148e9cac618a12
현성 <jin55789@naver.com>
2021-04-10 00:25:47 +0900

    🏗️ Fix donate data class

--- app/src/main/java/com/mashup/mobalmobal/data/dto/DonateDto.kt
+++ app/src/main/java/com/mashup/mobalmobal/data/dto/DonateDto.kt
@@ -3,10 +3,6 @@ package com.mashup.mobalmobal.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class DonateDto(
-    @SerializedName("donate_id") val donateId: Int,
-    @SerializedName("post_id") val postId: String,
-    @SerializedName("amount") val amount: String,
-    @SerializedName("user_id") val userId: Int,
-    @SerializedName("updatedAt") val updatedAt: String,
-    @SerializedName("createdAt") val createdAt: String,
+    @SerializedName("donate")
+    val donate: DonateInfoDto
 )

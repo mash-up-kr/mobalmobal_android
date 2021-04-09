b9129906d49c37afa13f6397c17244b526dec6ec
koba <koba1mobile@gmail.com>
2021-04-10 08:36:35 +0900

    🚀 Add request body

--- app/src/main/java/com/mashup/mobalmobal/ui/profile/data/service/ProfileService.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/profile/data/service/ProfileService.kt
@@ -1,14 +1,25 @@
 package com.mashup.mobalmobal.ui.profile.data.service
 
+import com.mashup.mobalmobal.ui.profile.data.dto.MyDonateResponseDto
 import com.mashup.mobalmobal.ui.profile.data.dto.MyPostResponseDto
 import com.mashup.mobalmobal.ui.profile.data.dto.ProfileResponseDto
 import io.reactivex.Single
+import okhttp3.RequestBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ProfileService {
     @GET("users")
     fun getUserInfo(): Single<ProfileResponseDto>
 
-    @GET("posts/my")
-    fun getMyDonations(): Single<MyPostResponseDto>
+    @POST("posts/my")
+    fun getMyPosts(
+        @Body
+        body: RequestBody
+    ): Single<MyPostResponseDto>
+
+    @GET("donate/my")
+    fun getMyDonates(): Single<MyDonateResponseDto>
 }

6c941b7df0db8e4519f3219f6fb0325541fe4464
koba <koba1mobile@gmail.com>
2021-05-04 00:04:16 +0900

    🏗️ Reformat code

--- app/src/main/java/com/mashup/mobalmobal/ui/donationdetail/presenter/DonationDetailFragment.kt
+++ app/src/main/java/com/mashup/mobalmobal/ui/donationdetail/presenter/DonationDetailFragment.kt
@@ -156,7 +156,7 @@ class DonationDetailFragment : BaseViewBindingFragment<FragmentDetailBinding>() 
                     val actualWidth = it.measuredWidth * actualRatio
                     width = actualWidth.toInt()
                 }
-                if(actualRatio > 0) visibility = ViewGroup.VISIBLE
+                if (actualRatio > 0) visibility = ViewGroup.VISIBLE
             }
         }
     }
